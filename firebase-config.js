8b4e8177b5aef35dfbf6812057a292b1f0c70c08
Jins George <geojins@gmail.com>
2025-11-17 13:13:33 -0800

    config

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,13 @@
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQbVnLH0A6uL-N43ptBVNI4hDB3BE2Rls",
-  authDomain: "smash-26679.firebaseapp.com",
-  projectId: "smash-26679",
-  storageBucket: "smash-26679.firebasestorage.app",
-  messagingSenderId: "877402703377",
-  appId: "1:877402703377:web:65db65464dbd385f6b53b0",
+  apiKey: "AIzaSyCQ_kIoe0NrjEmXDY-IWYpPpzLCkGeO3Ck",
+  authDomain: "smash-playground.firebaseapp.com",
+  projectId: "smash-playground",
+  storageBucket: "smash-playground.firebasestorage.app",
+  messagingSenderId: "996945866848",
+  appId: "1:996945866848:web:fbb8c8a1e55c3a38029305",
+  measurementId: "G-784D3NXFMB"
 };
 
 
